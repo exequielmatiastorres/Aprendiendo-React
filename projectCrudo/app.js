9bdabec7b8519842fd7b89ff700664e14d70147d
Exequiel Torres <exequieltorres0109@gmail.com>
2025-05-16 15:59:35 -0300

    primera entrega visual del proyecto Pedidos web

--- projectCrudo/app.js
+++ projectCrudo/app.js
@@ -1,7 +1,77 @@
-window.show.addEventListener('click', () => {
-    window.dialog.showModal();
-})
+// ventana dialog
+// window.show.addEventListener('click', () => {
+//     window.dialog.showModal();
+// })
 
-window.hide.addEventListener('click', () => {
-    window.dialog.close();
-})
+// window.hide.addEventListener('click', () => {
+//     window.dialog.close();
+// })
+
+
+
+//const menuIntera = document.querySelector('.menu');
+//const barra = document.querySelector('.barraNavegacion');
+//const change = menuIntera.addEventListener('click', cambiarDisplay);
+// menuIntera.addEventListener('click', cambiarDisplay2);
+// function cambiarDisplay() {
+//     if (barra.style.display === 'none') {
+//         barra.style.display = 'flex';
+//     } else {
+//         barra.style.display = 'none';
+//     }
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const lupa = document.querySelector('.searchTest');
+    const search = document.querySelector('.boxSearch');
+    const botonClose = document.querySelector('.buttonSearch');
+
+    lupa.addEventListener('click', cambiarDisplay2)
+    botonClose.addEventListener('click', closeBarra)
+    // botonClose.addEventListener('click', cambiarDisplay3)
+
+    function cambiarDisplay2() {
+        if (lupa.style.display = 'flex') {
+            search.style.display = 'flex'
+            lupa.style.display = 'none'
+        }
+    }
+    
+    function closeBarra() {
+        if (lupa.style.display = 'none') {
+            search.style.display = 'none'
+            lupa.style.display = 'flex'
+        }
+    }
+   
+});
+
+
+
+// function cambiarDisplay3() {
+//     console.log("Desde boton close")
+// }
+
+
+
+/* whatsapp funcionalidad */
+
+async function compartirObjeto() {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: 'Objeto interesante',
+          text: 'Echa un vistazo a esto:',
+          url: 'URL_DEL_OBJETO_O_PAGINA', // Opcional: URL relacionada
+          // files: [/* Array de objetos File */], // Para compartir archivos (soporte limitado)
+        });
+        console.log('Contenido compartido exitosamente');
+      } catch (error) {
+        console.log('Error al compartir', error);
+      }
+    } else {
+      console.log('La API de Compartir Web no es compatible');
+      // Ofrecer un método alternativo (como el enlace wa.me)
+    }
+  }
---
+++ projectCrudo/appTest.js
@@ -0,0 +1,36 @@
+//variables
+const botonMenos = document.querySelector('.logicButton1');
+const botonMas = document.querySelector('.logicButton2');
+const botonAddMore = document.querySelector('.logicButton3');
+const contador = document.querySelector('.logicNumber')
+const montoTotal = document.querySelector('.montoTotal')
+let contadorDe = 1;
+
+console.log(montoTotal)
+
+
+//eventos
+botonMenos.addEventListener('click', decremento);
+botonMas.addEventListener('click', incremento);
+
+
+function decremento() {
+    if (contadorDe <= 1) {
+        console.log(`imposible bajar la cantidad`)
+    } else {
+        const seteo = Number.parseInt(montoTotal.innerHTML);
+        contadorDe--
+        contador.innerHTML = contadorDe;
+        montoTotal.innerHTML = contadorDe * 20000;
+    } return
+}
+
+function incremento() {
+    // 
+    contadorDe++
+    contador.innerHTML = contadorDe;
+    montoTotal.innerHTML = contadorDe * 20000;
+}
+// 20000 tiene que ser una variable
+
+
